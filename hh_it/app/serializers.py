b9af53a3056231a9cb609b7f31dfb6dac128f03a
hiki7 <rsaliyev03@gmail.com>
2024-09-29 19:06:57 +0500

    add: serializer for Technology

--- hh_it/app/serializers.py
+++ hh_it/app/serializers.py
@@ -6,3 +6,8 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = ['id', 'name', 'company_description', 'website', 'location']
+
+class TechnologySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
+        fields = ['id', 'technology_name']
