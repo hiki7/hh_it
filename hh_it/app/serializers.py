86a0ab71f1542319cbfea2506d7c60e87bdbf79a
hiki7 <rsaliyev03@gmail.com>
2024-09-29 19:07:24 +0500

    add: serializer for Vacancy

--- hh_it/app/serializers.py
+++ hh_it/app/serializers.py
@@ -11,3 +11,13 @@ class TechnologySerializer(serializers.ModelSerializer):
     class Meta:
         model = Technology
         fields = ['id', 'technology_name']
+
+class VacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = [
+            'id', 'position_name', 'salary_start', 'salary_end',
+            'required_experience_year_start', 'required_experience_year_end',
+            'position_description', 'company', 'location', 'posted_date',
+            'employment_type', 'technology'
+        ]
