897ee893b5bafcbbe1ffa83aba1a8f4cfd088876
hiki7 <rsaliyev03@gmail.com>
2024-09-30 15:44:11 +0500

    change: the order of models in the dict.py, and the data type of 'location' in Company model

--- hh_it/app/models/dict.py
+++ hh_it/app/models/dict.py
@@ -3,69 +3,6 @@ from .abstract import TimestampModel
 from django.utils.translation import gettext_lazy as _
 
 
-class Company(TimestampModel):
-    """Компания"""
-    id: int
-    name: str = models.CharField(
-        max_length=255,
-        verbose_name='Компания'
-    )
-    company_description: str = models.TextField(
-        null=False,
-        blank=False,
-        verbose_name='Текст компании'
-    )
-    website: str = models.URLField(
-        null=True,
-        blank=True,
-        verbose_name='Веб-сайт компании'
-    )
-    location: str = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True,
-        verbose_name='Локация компании'
-    )
-
-    class Meta:
-        verbose_name_plural = _("Компании")
-        verbose_name = _("Компания")
-
-    def __str__(self):
-        return self.name
-
-
-class Technology(TimestampModel):
-    """Технологии (Python, Docker, React, etc)"""
-    id: int
-    technology_name: str = models.CharField(
-        max_length=255,
-        null=False,
-        blank=False,
-        verbose_name='Технология'
-    )
-
-    class Meta:
-        verbose_name_plural = _("Технологии")
-        verbose_name = _("Технология")
-
-    def __str__(self):
-        return self.technology_name
-
-
-class Position(TimestampModel):
-    """Позиция"""
-    id: int
-    name: str = models.CharField(max_length=255, null=False, blank=False, unique=True, verbose_name='Позиция')
-
-    class Meta:
-        verbose_name_plural = _("Позиции")
-        verbose_name = _("Позиция")
-
-    def __str__(self):
-        return self.name
-
-
 class Country(TimestampModel):
     """Страна"""
     id: int
@@ -158,3 +95,65 @@ class Location(TimestampModel):
 
     def __str__(self):
         return f'{self.country.name}'
+
+
+class Company(TimestampModel):
+    """Компания"""
+    id: int
+    name: str = models.CharField(
+        max_length=255,
+        verbose_name='Компания'
+    )
+    company_description: str = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name='Текст компании'
+    )
+    website: str = models.URLField(
+        null=True,
+        blank=True,
+        verbose_name='Веб-сайт компании'
+    )
+    location: Location = models.ForeignKey(
+        'Location',
+        on_delete=models.CASCADE,
+        verbose_name='Локация компании'
+    )
+
+    class Meta:
+        verbose_name_plural = _("Компании")
+        verbose_name = _("Компания")
+
+    def __str__(self):
+        return self.name
+
+
+class Technology(TimestampModel):
+    """Технологии (Python, Docker, React, etc)"""
+    id: int
+    technology_name: str = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        verbose_name='Технология'
+    )
+
+    class Meta:
+        verbose_name_plural = _("Технологии")
+        verbose_name = _("Технология")
+
+    def __str__(self):
+        return self.technology_name
+
+
+class Position(TimestampModel):
+    """Позиция"""
+    id: int
+    name: str = models.CharField(max_length=255, null=False, blank=False, unique=True, verbose_name='Позиция')
+
+    class Meta:
+        verbose_name_plural = _("Позиции")
+        verbose_name = _("Позиция")
+
+    def __str__(self):
+        return self.name
