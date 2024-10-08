c77e45e54670e641192394ae6c88c3e1016211c1
hiki7 <rsaliyev03@gmail.com>
2024-10-08 20:48:12 +0500

    add: hidden_vacancies in admin panel

--- hh_it/app/admin.py
+++ hh_it/app/admin.py
@@ -1,70 +1,105 @@
 from django.contrib import admin
-from .models import (Company, Technology, Position,
-                     Country, City, Street, Location, Vacancy, EmploymentType)
+from .models import (
+    Company,
+    Technology,
+    Position,
+    Country,
+    City,
+    Street,
+    Location,
+    Vacancy,
+    EmploymentType,
+    HiddenVacancies,
+    HiddenCompanies,
+)
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'website', 'location')
-    search_fields = ('name', 'location__country__name', 'location__city__name', 'location__street__name')
-    list_filter = ('location__country', 'location__city')
-    ordering = ('name',)
+    list_display = ("name", "website", "location")
+    search_fields = (
+        "name",
+        "location__country__name",
+        "location__city__name",
+        "location__street__name",
+    )
+    list_filter = ("location__country", "location__city")
+    ordering = ("name",)
 
 
 @admin.register(Technology)
 class TechnologyAdmin(admin.ModelAdmin):
-    list_display = ('technology_name',)
-    search_fields = ('technology_name',)
-    ordering = ('technology_name',)
+    list_display = ("technology_name",)
+    search_fields = ("technology_name",)
+    ordering = ("technology_name",)
 
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    ordering = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
 
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    ordering = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
 
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country')
-    search_fields = ('name', 'country__name')
-    list_filter = ('country',)
-    ordering = ('name',)
+    list_display = ("name", "country")
+    search_fields = ("name", "country__name")
+    list_filter = ("country",)
+    ordering = ("name",)
 
 
 @admin.register(Street)
 class StreetAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    ordering = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('country', 'city', 'street')
-    search_fields = ('country__name', 'city__name', 'street__name')
-    list_filter = ('country', 'city')
-    ordering = ('country', 'city', 'street')
+    list_display = ("country", "city", "street")
+    search_fields = ("country__name", "city__name", "street__name")
+    list_filter = ("country", "city")
+    ordering = ("country", "city", "street")
 
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
-    list_display = ('position_name', 'company', 'salary_start', 'salary_end', 'currency', 'is_active')
-    search_fields = ('position_name__name', 'company__name', 'location__country__name', 'technology__technology_name')
-    list_filter = ('company', 'currency', 'is_active', 'employment_type', 'technology')
-    ordering = ('position_name', 'company')
+    list_display = (
+        "position_name",
+        "company",
+        "salary_start",
+        "salary_end",
+        "currency",
+        "is_active",
+    )
+    search_fields = (
+        "position_name__name",
+        "company__name",
+        "location__country__name",
+        "technology__technology_name",
+    )
+    list_filter = ("company", "currency", "is_active", "employment_type", "technology")
+    ordering = ("position_name", "company")
 
 
 @admin.register(EmploymentType)
 class EmploymentTypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    ordering = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(HiddenVacancies)
+class HiddenVacanciesAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "vacancy_id")
+    search_fields = ("user_id", "vacancy_id")
+    ordering = ("user_id", "vacancy_id")
