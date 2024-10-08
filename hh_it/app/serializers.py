24f9f270a85c2fc1d893452239904bcd1a777aed
hiki7 <rsaliyev03@gmail.com>
2024-10-08 20:50:29 +0500

    change: properly styled the code

--- hh_it/app/serializers.py
+++ hh_it/app/serializers.py
@@ -1,29 +1,41 @@
 from rest_framework import serializers
-from .models import Company, Technology, Vacancy, Country, City, Street, Location, Position, EmploymentType
+from .models import (
+    Company,
+    Technology,
+    Vacancy,
+    Country,
+    City,
+    Street,
+    Location,
+    Position,
+    EmploymentType,
+    HiddenCompanies,
+    HiddenVacancies,
+)
 
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ['id', 'name', 'country']
+        fields = ["id", "name", "country"]
 
 
 class StreetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Street
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ['id', 'country', 'city', 'street']
+        fields = ["id", "country", "city", "street"]
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -31,25 +43,25 @@ class CompanySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Company
-        fields = ['id', 'name', 'company_description', 'website', 'location']
+        fields = ["id", "name", "company_description", "website", "location"]
 
 
 class TechnologySerializer(serializers.ModelSerializer):
     class Meta:
         model = Technology
-        fields = ['id', 'technology_name']
+        fields = ["id", "technology_name"]
 
 
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class EmploymentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmploymentType
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class VacancySerializer(serializers.ModelSerializer):
@@ -62,8 +74,18 @@ class VacancySerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacancy
         fields = [
-            'id', 'position_name', 'salary_start', 'salary_end', 'currency', 'get_salary_range',
-            'required_experience_year_start', 'required_experience_year_end',
-            'position_description', 'company', 'location',
-            'employment_type', 'technology', 'is_active'
+            "id",
+            "position_name",
+            "salary_start",
+            "salary_end",
+            "currency",
+            "get_salary_range",
+            "required_experience_year_start",
+            "required_experience_year_end",
+            "position_description",
+            "company",
+            "location",
+            "employment_type",
+            "technology",
+            "is_active",
         ]
